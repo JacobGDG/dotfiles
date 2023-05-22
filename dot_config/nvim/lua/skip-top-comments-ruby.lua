422269a10e3529bc84fb8bc34be536bf4af0a1a3
JacobGDG <jacob.greenwood@gmail.com>
2023-05-22 09:35:48 +0100

    Add ruby command to skip top comments

--- dot_config/nvim/lua/skip-top-comments-ruby.lua
+++ dot_config/nvim/lua/skip-top-comments-ruby.lua
@@ -1,2 +1,28 @@
--- empty SkipTopCommentsRuby function to prevent error
-function SkipTopCommentsRuby() end
+-- we use https://github.com/ctran/annotate_models to annotate our models
+-- this is a pain to scroll past
+-- this function jumps to the first line of the model that is not a comment
+
+function SkipTopCommentsRuby()
+  -- Check if the current buffer is a Ruby file
+  if vim.bo.filetype == 'ruby' then
+
+    -- Loop through each line in the buffer
+    for i = 1, vim.fn.line('$') do
+
+      -- Get the current line
+      local line = vim.fn.getline(i)
+
+      -- Check if the line is a comment
+      if line:match('^%s*#') then
+        -- If it is a comment, continue to the next line
+        goto continue
+      else
+        -- If it is not a comment, set the mark at this line
+        vim.fn.setpos('.', {0, i, 0, 0})
+        break
+      end
+
+      ::continue::
+    end
+  end
+end
