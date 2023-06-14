2bdd850bf70e99a7cab6c84304dbff8479f5a33a
JacobGDG <jacob.greenwood@gmail.com>
2023-06-14 10:37:45 +0100

    Webp convert script + Copilot hotfix
    Need to fix tab assignment for LSP complete and copilot. I thought I
    had by assigning Ctrl-Tab for coplit but.. apparently not

--- dot_config/nvim/plugins/cmp.lua
+++ dot_config/nvim/plugins/cmp.lua
@@ -35,10 +35,10 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<Tab>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    })
+   -- ['<Tab>'] = cmp.mapping.confirm({
+   --   behavior = cmp.ConfirmBehavior.Insert,
+   --   select = true,
+   -- })
   },
   -- Installed sources:
   sources = {
