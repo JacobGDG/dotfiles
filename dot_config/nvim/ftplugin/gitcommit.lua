4e99c51ad81b46931a45ae4acf6f93af7cee3d74
JacobGDG <jacob.greenwood@gmail.com>
2024-11-16 16:38:30 +0000

    Improve gitcommit process with plugin

--- dot_config/nvim/ftplugin/gitcommit.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_set_option_value("textwidth", 72, {})
-vim.api.nvim_set_option_value("colorcolumn", "51,72", {})
-vim.api.nvim_set_option_value("spell", true, {})
--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -12,7 +12,7 @@ return {
     init = function()
       -- See: https://github.com/rhysd/committia.vim#variables
       vim.g.committia_min_window_width = 30
-      vim.g.committia_edit_window_width = 75
+      vim.g.committia_edit_window_width = 80
     end,
     config = function()
       vim.g.committia_hooks = {
@@ -27,12 +27,32 @@ return {
           end
           map('n', 'q', '<cmd>quit<CR>')
           map('i', '<C-d>', '<Plug>(committia-scroll-diff-down-half)')
+          map('n', '<C-d>', '<Plug>(committia-scroll-diff-down-half)')
           map('i', '<C-u>', '<Plug>(committia-scroll-diff-up-half)')
-          map('i', '<C-f>', '<Plug>(committia-scroll-diff-down-page)')
-          map('i', '<C-b>', '<Plug>(committia-scroll-diff-up-page)')
-          map('i', '<C-j>', '<Plug>(committia-scroll-diff-down)')
-          map('i', '<C-k>', '<Plug>(committia-scroll-diff-up)')
+          map('n', '<C-u>', '<Plug>(committia-scroll-diff-up-half)')
+          vim.cmd("highlight ColorColumn ctermbg=0 guibg=DarkGreen")
+          vim.api.nvim_set_option_value("colorcolumn", "51", {})
         end,
+        status_open = function()
+          local opts = {
+            buffer = vim.api.nvim_get_current_buf(),
+            silent = true,
+          }
+          local function map(mode, lhs, rhs)
+            vim.keymap.set(mode, lhs, rhs, opts)
+          end
+          map('n', 'q', '<cmd>quit<CR>')
+        end,
+        diff_open = function()
+          local opts = {
+            buffer = vim.api.nvim_get_current_buf(),
+            silent = true,
+          }
+          local function map(mode, lhs, rhs)
+            vim.keymap.set(mode, lhs, rhs, opts)
+          end
+          map('n', 'q', '<cmd>quit<CR>')
+        end
       }
     end,
   },
